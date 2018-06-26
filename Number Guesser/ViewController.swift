a9855e84c8449d5fa2fd6ff410a5b8f64a0e749d
gness1804 <graham.nessler@gmail.com>
2018-06-25 21:07:39 -0500

    Add validity check for user number.

--- Number Guesser/ViewController.swift
+++ Number Guesser/ViewController.swift
@@ -10,17 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     var userNumber: Int = 0
+    
     var computerNumber: Int = 0
     
     @IBOutlet weak var mainOutput: UILabel!
     
-    @IBAction func mainButton(_ sender: Any) {
-        userNumber = Int(numberInput.text!)!
-
-    }
-    
     @IBOutlet weak var numberInput: UITextField!
     
+    func inputIsValid(input: Int) -> Bool {
+        if input < 0 || input > 100 {
+            print("Error: your number must be between 0 and 100. Please try again.")
+            return false
+        }
+        return true
+    }
+    
+    // TODO: come back to nil check for no input
+    @IBAction func mainButton(_ sender: Any) {
+        userNumber = (Int(numberInput.text!))!
+        computerNumber = Int(arc4random_uniform(101))
+        print(userNumber)
+        print(computerNumber)
+//        if userNumber == nil {
+//            print("Error: please enter a number.")
+//            return
+//        }
+        if inputIsValid(input: userNumber) {
+            print("Input is valid.")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
