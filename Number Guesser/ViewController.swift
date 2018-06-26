9c7b20a44b119ea28ac84aa492905b41fa775228
gness1804 <graham.nessler@gmail.com>
2018-06-25 20:48:28 -0500

    Capture user number

--- Number Guesser/ViewController.swift
+++ Number Guesser/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var userNumber: Int = 0
+    var computerNumber: Int = 0
+    
+    @IBOutlet weak var mainOutput: UILabel!
+    
+    @IBAction func mainButton(_ sender: Any) {
+        userNumber = Int(numberInput.text!)!
 
+    }
+    
+    @IBOutlet weak var numberInput: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
